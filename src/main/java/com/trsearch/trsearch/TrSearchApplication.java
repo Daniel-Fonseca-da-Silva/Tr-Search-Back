2f78d4728030bba7147645b2beab1b29f4e54e90
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-09-20 11:59:06 -0300

    Created project with Spring Initializr

---
+++ src/main/java/com/trsearch/trsearch/TrSearchApplication.java
@@ -0,0 +1,14 @@
+package com.trsearch.trsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrSearchApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/trsearch/trsearch/TrSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trsearch.trsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
