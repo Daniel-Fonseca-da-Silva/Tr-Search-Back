9e5af03e3a524c43c49c72b16b5d2fee7d215bf7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-24 23:54:24 -0300

    Created the repository for UserCorp

---
+++ src/main/java/com/trsearch/trsearch/service/UserCorpRepository.java
@@ -0,0 +1,9 @@
+package com.trsearch.trsearch.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trsearch.trsearch.model.UserCorp;
+
+public interface UserCorpRepository extends JpaRepository<UserCorp, Long>{
+	
+}
