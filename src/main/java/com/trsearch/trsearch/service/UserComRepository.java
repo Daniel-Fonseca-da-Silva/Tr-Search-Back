6d0072acce1f93ee666903ab71cc5a9b95cb3340
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-25 06:05:51 -0300

    Created the repository for UserCom

---
+++ src/main/java/com/trsearch/trsearch/service/UserComRepository.java
@@ -0,0 +1,9 @@
+package com.trsearch.trsearch.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trsearch.trsearch.model.UserCom;
+
+public interface UserComRepository extends JpaRepository<UserCom, Long>{
+
+}
