30239b18cd0947969a468537b94c8aa5a0d830ca
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-25 06:05:05 -0300

    Created the service for UserCom

---
+++ src/main/java/com/trsearch/trsearch/service/UserComService.java
@@ -0,0 +1,12 @@
+package com.trsearch.trsearch.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserComService {
+	
+	@Autowired
+	UserComRepository repo;
+	
+}
