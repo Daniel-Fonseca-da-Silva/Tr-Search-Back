4c5bba0b306e46f6d858407ab5c38db33a35d751
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-25 06:21:52 -0300

    Created getUserComById

--- src/main/java/com/trsearch/trsearch/controller/UserComController.java
+++ src/main/java/com/trsearch/trsearch/controller/UserComController.java
@@ -1,10 +1,16 @@
 package com.trsearch.trsearch.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.trsearch.trsearch.model.UserCom;
 import com.trsearch.trsearch.service.UserComService;
 
 @CrossOrigin("*")
@@ -14,5 +20,11 @@ public class UserComController {
 	
 	@Autowired
 	UserComService service;
+	
+	@GetMapping("/{id}")
+	ResponseEntity<?> getUserComById(@PathVariable("id") Long id) {
+		Optional<UserCom> userCom = service.getUserComById(id);
+		return userCom.isPresent() ? ResponseEntity.ok(userCom) : ResponseEntity.notFound().build();
+	}
 	
 }
--- src/main/java/com/trsearch/trsearch/service/UserComService.java
+++ src/main/java/com/trsearch/trsearch/service/UserComService.java
@@ -1,12 +1,20 @@
 package com.trsearch.trsearch.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.trsearch.trsearch.model.UserCom;
 
 @Service
 public class UserComService {
 	
 	@Autowired
 	UserComRepository repo;
+
+	public Optional<UserCom> getUserComById(Long id) {
+		return repo.findById(id);
+	}
 	
 }
