c3e5dd1a664478c1c0513bbd1c2c2db4ff7c0fe3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-03 15:42:20 -0300

    Was created the TestDeleteUserCorp

--- src/test/java/com/trsearch/trsearch/TrSearchApplicationTests.java
+++ src/test/java/com/trsearch/trsearch/TrSearchApplicationTests.java
@@ -1,9 +1,10 @@
 package com.trsearch.trsearch;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,27 +14,30 @@ import com.trsearch.trsearch.model.UserCorp;
 import com.trsearch.trsearch.service.UserCorpRepository;
 import com.trsearch.trsearch.service.UserCorpService;
 
+import java.util.Optional;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TrSearchApplicationTests {
-	
+
 	@MockBean
-    public UserCorpRepository repository;
-	
+	public UserCorpRepository repository;
+
 	@Autowired
-    public UserCorpService service;
-	
+	public UserCorpService service;
+
+
 	@Test
-	public void TestCreateUserCorp() {
-		
+	public void test_CreateUserCorp() {
+
 		UserCorp userCorp = new UserCorp();
-		
+
 		userCorp.setId(1L);
 		userCorp.setNameCorp("Levi e Benício Marcenaria ME");
 		userCorp.setCnpjCorp("39.219.520/0001-25");
 		userCorp.setLoginCorp("admin");
 		userCorp.setEmailCorp("cceciliamayapeixoto@amure.com.br");
 		userCorp.setPasswordCorp("nTxrKgY$L@");
-		
+
 		UserCorp userCorpp = service.createUserCorp(userCorp);
 		assertNotNull(userCorpp);
 		assertEquals(userCorpp, userCorp);
@@ -47,9 +51,10 @@ class TrSearchApplicationTests {
 		service.deleteUserCorp(userCorpp.getId());
 		assertFalse(service.getUserCorpById(userCorpp.getId()).isPresent());
 	}
-	
+
+
 	@Test
-	public void TestGetIdUserCorpp() {
+	public void test_GetIdUserCorp() {
 		UserCorp userCorp = new UserCorp();
 		userCorp.setId(1L);
 		userCorp.setNameCorp("Levi e Benício Marcenaria ME");
@@ -57,11 +62,28 @@ class TrSearchApplicationTests {
 		userCorp.setLoginCorp("admin");
 		userCorp.setEmailCorp("cceciliamayapeixoto@amure.com.br");
 		userCorp.setPasswordCorp("nTxrKgY$L@");
-		
+
 		UserCorp userCorpp = service.createUserCorp(userCorp);
 		assertNotNull(service.getUserCorpById(userCorpp.getId()));
+		assertEquals(Optional.of(1L), Optional.of(userCorpp.getId()));
+		service.deleteUserCorp(1L);
+	}
+
+	@Test
+	public void test_DeleteUserCorp() {
+		UserCorp userCorp = new UserCorp();
+		userCorp.setId(1L);
+		userCorp.setNameCorp("Levi e Benício Marcenaria ME");
+		userCorp.setCnpjCorp("39.219.520/0001-25");
+		userCorp.setLoginCorp("admin");
+		userCorp.setEmailCorp("cceciliamayapeixoto@amure.com.br");
+		userCorp.setPasswordCorp("nTxrKgY$L@");
+
+		UserCorp userCorpp = service.createUserCorp(userCorp);
+
 		service.deleteUserCorp(userCorpp.getId());
+		Assertions.assertNotEquals(1L, service.getUserCorpById(userCorpp.getId()));
+		assertTrue(service.getUserCorpById(userCorpp.getId()).isEmpty());
 	}
 
 }
-
