656649e8c532a7096f1640f1690736eb56ba0369
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-05 18:09:19 -0300

    Configured SecurityConfig

---
+++ src/main/java/com/trsearch/trsearch/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.trsearch.trsearch.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
