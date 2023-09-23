e260b45cafd0aaeaf8a4b2fc0126800c93b2b91b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-22 23:11:25 -0300

    feature: Created the base of project

---
+++ src/main/java/com/dafon/trsearchback/TrSearchBackApplication.java
@@ -0,0 +1,13 @@
+package com.dafon.trsearchback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrSearchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrSearchBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dafon/trsearchback/TrSearchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dafon.trsearchback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrSearchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
