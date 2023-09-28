9df765118415562b5edeab0b468303cdeae955cc
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-27 22:39:15 -0300

    feature: Added the law of path to access login page

--- src/main/java/com/dafon/trsearchback/controller/AuthenticationController.java
+++ src/main/java/com/dafon/trsearchback/controller/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.security.authentication.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/login")
+@RequestMapping("api/v1/login")
 public class AuthenticationController {
 
     @Autowired
--- src/main/java/com/dafon/trsearchback/controller/TestWork.java
+++ src/main/java/com/dafon/trsearchback/controller/TestWork.java
@@ -1,14 +1,22 @@
 package com.dafon.trsearchback.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class TestWork {
 
+    @GetMapping("test")
+    @Secured({"ROLE_ADMIN"})
+    public String work() {
+        return "ADMIN spring!";
+    }
+
     @GetMapping("work")
+    @Secured({"ROLE_USER"})
     public String test() {
-        return "Worked spring!";
+        return "USER spring!";
     }
 
 }
