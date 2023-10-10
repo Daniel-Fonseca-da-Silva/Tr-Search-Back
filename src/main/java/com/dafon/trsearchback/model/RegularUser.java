23667dddda90070c3af53e314fd706f643d643fd
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-10-10 20:03:57 -0300

    feature: Added new constructors inside regular user

--- src/main/java/com/dafon/trsearchback/model/RegularUser.java
+++ src/main/java/com/dafon/trsearchback/model/RegularUser.java
@@ -1,20 +1,25 @@
 package com.dafon.trsearchback.model;
 
+
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDate;
+import java.io.Serializable;
+import java.time.*;
 import java.util.*;
+
+import com.dafon.trsearchback.dto.DatasRegularUserDto;
+import com.dafon.trsearchback.dto.DatasRegularUserUpdateDto;
 
 @Table(name = "regular_users")
 @Entity(name = "RegularUser")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegularUser extends User implements UserDetails {
+public class RegularUser extends User implements UserDetails, Serializable {
 
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDate birthday;
@@ -26,6 +31,46 @@ public class RegularUser extends User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public RegularUser(DatasRegularUserDto dto) {
+        super.setName(dto.name());
+        super.setCellphone(dto.cellphone());
+        super.setPassword(dto.password());
+        super.setEmail(dto.email());
+        super.setPhoto(dto.photo());
+        super.setDocumentation(dto.documentation());
+        super.setStatus(true);
+        super.setCreatedAt(LocalDateTime.now());
+        super.setRoles("ROLE_USER");
+        this.birthday = dto.birthday();
+        this.gender = dto.gender();
+    }
+
+    public void updateInformation(DatasRegularUserUpdateDto dto) {
+        if (dto.name() != null)
+            this.setName(dto.name());
+
+        if (dto.email() != null)
+            this.setEmail(dto.email());
+
+        if (dto.cellphone() != null)
+            this.setCellphone(dto.cellphone());
+
+        if (dto.password() != null)
+            this.setPassword(dto.password());
+
+        if (dto.photo() != null)
+            this.setPhoto(dto.photo());
+
+        if (dto.birthday() != null)
+            this.birthday = dto.birthday();
+
+        if (dto.documentation() != null)
+            this.setDocumentation(dto.documentation());
+
+        if (dto.gender() != null)
+            gender = dto.gender();
     }
 
     @Override
@@ -57,4 +102,5 @@ public class RegularUser extends User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
 }
