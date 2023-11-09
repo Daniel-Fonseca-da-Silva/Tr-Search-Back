251eb67e40bdafb2e16337f2eb1052be06fa6b0b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-09 14:47:37 -0300

    fix: Added recommended dependency injection to methods

--- src/main/java/com/dafon/trsearchback/exception/TreatmentOfErrors.java
+++ src/main/java/com/dafon/trsearchback/exception/TreatmentOfErrors.java
@@ -6,7 +6,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestControllerAdvice
 public class TreatmentOfErrors {
 
--- src/main/java/com/dafon/trsearchback/model/Premium.java
+++ src/main/java/com/dafon/trsearchback/model/Premium.java
@@ -1,6 +1,7 @@
 package com.dafon.trsearchback.model;
 
 public enum Premium {
+    FREE,
     SIMPLE,
     BRONZE,
     SILVER,
