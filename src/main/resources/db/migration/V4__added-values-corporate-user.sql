0c1eeef91ac00bc19f254722675f319c64daa960
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-09 14:44:51 -0300

    feature: Changed fields of database

--- src/main/resources/db/migration/V4__added-values-corporate-user.sql
+++ src/main/resources/db/migration/V4__added-values-corporate-user.sql
@@ -1,63 +1,14 @@
-INSERT INTO corporate_users (
-    id,
-    cellphone,
-    created_at,
-    documentation,
-    email,
-    name,
-    last_login,
-    password,
-    photo,
-    roles,
-    status,
-    updated_at,
-    premium,
-    telephone
-) VALUES (
-    'e76d64d8-2f7d-4e35-ba7e-73c240e19a75',
-    '1234567890',
-    '2023-09-24 14:30:00',
-    '123456789012345',
-    'exemplo1@email.com',
-    'Primeiro1',
-    '2023-09-24 14:35:00',
-    'senha_hashed1',
-    'caminho_da_foto1.jpg',
-    'role1,role2',
-    true,
-    '2023-09-24 14:30:00',
-    'SILVER',
-    '987654321'
-);
-
-INSERT INTO corporate_users (
-    id,
-    cellphone,
-    created_at,
-    documentation,
-    email,
-    name,
-    last_login,
-    password,
-    photo,
-    roles,
-    status,
-    updated_at,
-    premium,
-    telephone
-) VALUES (
-    'a4f6e2a1-1b3c-4d5e-6f7a-8a9d0b1c2e3f',
-    '9876543210',
-    '2023-09-25 10:45:00',
-    '543210987654321',
-    'exemplo2@email.com',
-    'Primeiro2',
-    '2023-09-25 10:50:00',
-    'senha_hashed2',
-    'caminho_da_foto2.jpg',
-    'role3,role4',
-    true,
-    '2023-09-25 10:45:00',
-    'GOLD',
-    '123456789'
+INSERT INTO corporate_users (id, cellphone, created_at, documentation, email, name, password, roles, status, updated_at, premium, telephone)
+VALUES
+(
+    '1ec34f21-1732-4ab2-a62b-eb35d1b62194', '123-456-7890', CURRENT_TIMESTAMP, '123456789012345', 'usuario1@example.com', 'Usuário Comum', '$2a$12$XEqhEMh2sDpLvytozFozvunvhqQ70jEtB1zmMlAV8IrqE8XnlzTNa', 'ROLE_CORPORATE', TRUE, CURRENT_TIMESTAMP, 'FREE', '123-456-7890'
+),
+(
+    '2ec34f21-1732-4ab2-a62b-eb35d1b62195', '987-654-3210', CURRENT_TIMESTAMP, '987654321098765', 'premium1@example.com', 'Usuário Premium Bronze', '$2a$12$XEqhEMh2sDpLvytozFozvunvhqQ70jEtB1zmMlAV8IrqE8XnlzTNa', 'ROLE_CORPORATE', TRUE, CURRENT_TIMESTAMP, 'BRONZE', '987-654-3210'
+),
+(
+    '3ec34f21-1732-4ab2-a62b-eb35d1b62196', '555-555-5555', CURRENT_TIMESTAMP, '555555555555555', 'premium2@example.com', 'Usuário Premium Prata', '$2a$12$XEqhEMh2sDpLvytozFozvunvhqQ70jEtB1zmMlAV8IrqE8XnlzTNa', 'ROLE_CORPORATE', TRUE, CURRENT_TIMESTAMP, 'SILVER', '555-555-5555'
+),
+(
+    '4ec34f21-1732-4ab2-a62b-eb35d1b62197', '111-222-3333', CURRENT_TIMESTAMP, '111122223333444', 'premium3@example.com', 'Usuário Premium Ouro', '$2a$12$XEqhEMh2sDpLvytozFozvunvhqQ70jEtB1zmMlAV8IrqE8XnlzTNa', 'ROLE_CORPORATE', TRUE, CURRENT_TIMESTAMP, 'GOLD', '111-222-3333'
 );
