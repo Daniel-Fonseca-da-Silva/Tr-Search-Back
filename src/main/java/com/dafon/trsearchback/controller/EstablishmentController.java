5ec8fd6aa3e3ab73180b5516ffb78ebcdf7dd4fb
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-11-16 20:29:06 -0300

    feature: Created crud of establishment

---
+++ src/main/java/com/dafon/trsearchback/controller/EstablishmentController.java
@@ -0,0 +1,94 @@
+package com.dafon.trsearchback.controller;
+
+import com.dafon.trsearchback.dto.*;
+import com.dafon.trsearchback.interfaces.BaseCrud;
+import com.dafon.trsearchback.model.Category;
+import com.dafon.trsearchback.model.Establishment;
+import com.dafon.trsearchback.service.EstablishmentService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@SecurityRequirement(name = "bearer-key")
+@RequestMapping("api/v1/establishment")
+public class EstablishmentController implements BaseCrud<CreateEstablishmentDto, UpdateEstablishmentDto, ShowDatasEstablishmentDto> {
+
+    private final EstablishmentService establishmentService;
+
+    @Autowired
+    public EstablishmentController(EstablishmentService establishmentService) {
+     this.establishmentService = establishmentService;
+    }
+
+    @PostMapping
+    @Transactional
+    @Secured({"ROLE_CORPORATE"})
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<CreateEstablishmentDto> createObject(@RequestBody @Valid CreateEstablishmentDto dto, UriComponentsBuilder uriBuilder) {
+        var establishment = new Establishment(dto);
+
+        establishmentService.createElement(establishment);
+
+        var uri = uriBuilder.path("/api/v1/establishment/{id}").buildAndExpand(establishment.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping
+    @Transactional
+    @Secured({"ROLE_CORPORATE"})
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<UpdateEstablishmentDto> updateObject(@RequestBody @Valid UpdateEstablishmentDto dto) {
+        var establishmentUpdated = establishmentService.updateElement(dto);
+        return ResponseEntity.ok(new UpdateEstablishmentDto(establishmentUpdated));
+    }
+
+    @GetMapping("/category/{category}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<ShowDatasEstablishmentDto> findObjectByCategory(@PathVariable Category category) {
+        var establishment = establishmentService.findElementByCategory(category);
+        return ResponseEntity.ok(new ShowDatasEstablishmentDto(establishment));
+    }
+
+    @GetMapping("/name/{name}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<ShowDatasEstablishmentDto> findObject(@PathVariable String name) {
+        var establishment = establishmentService.findElement(name);
+        return ResponseEntity.ok(new ShowDatasEstablishmentDto(establishment));
+    }
+
+    @GetMapping("/code/{code}")
+    @Secured({"ROLE_CORPORATE"})
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<ShowDatasEstablishmentCorporateDto> findObjectByCode(@PathVariable String code) {
+        var establishment = establishmentService.findElementByCode(code);
+        return ResponseEntity.ok(new ShowDatasEstablishmentCorporateDto(establishment));
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<Page<ShowDatasEstablishmentDto>> findObjects(Pageable pagination) {
+        var page = establishmentService.findElements(pagination);
+        return ResponseEntity.ok(page);
+    }
+
+    @DeleteMapping("/{code}")
+    @Secured({"ROLE_CORPORATE"})
+    @Transactional
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<Void> removeObject(@PathVariable String code) {
+        establishmentService.removeElement(code);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ src/main/java/com/dafon/trsearchback/model/Establishment.java
@@ -0,0 +1,183 @@
+package com.dafon.trsearchback.model;
+
+import com.dafon.trsearchback.dto.CreateEstablishmentDto;
+import com.dafon.trsearchback.dto.UpdateEstablishmentDto;
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "establishments")
+@Entity(name = "Establishment")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Establishment implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false, unique = true, columnDefinition = "uuid")
+    private UUID id;
+
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = true)
+    private String photo;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @NotBlank
+    @Column(length = 20, nullable = false)
+    private String telephone;
+
+    @NotBlank
+    @Column(length = 20, nullable = false)
+    private String cellphone;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(length = 2, nullable = false)
+    private Boolean status;
+
+    @CreationTimestamp
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    @Column(length = 255)
+    private String picture1;
+
+    @Column(length = 255)
+    private String picture2;
+
+    @Column(length = 255)
+    private String picture3;
+
+    @Column(length = 255)
+    private String picture4;
+
+    @Column(length = 255)
+    private String picture5;
+
+    @Column(length = 255)
+    private String picture6;
+
+    @Column(length = 255)
+    private String picture7;
+
+    @Column(length = 255)
+    private String picture8;
+
+    @Column(length = 255)
+    private String picture9;
+
+    @Column(length = 255)
+    private String picture10;
+
+    public Establishment(CreateEstablishmentDto dto) {
+        this.name = dto.name();
+        this.photo = dto.photo();
+        this.description = dto.description();
+        this.telephone = dto.telephone();
+        this.cellphone = dto.cellphone();
+        this.email = dto.email();
+        this.category = dto.category();
+        this.code = UuidCreator.getTimeOrderedEpoch().toString();
+        this.status = true;
+        this.picture1 = dto.picture1();
+        this.picture2 = dto.picture2();
+        this.picture3 = dto.picture3();
+        this.picture4 = dto.picture4();
+        this.picture5 = dto.picture5();
+        this.picture6 = dto.picture6();
+        this.picture7 = dto.picture7();
+        this.picture8 = dto.picture8();
+        this.picture9 = dto.picture9();
+        this.picture10 = dto.picture10();
+    }
+
+    public void desactivate() {
+        this.status = false;
+    }
+
+    public void updateInformation(UpdateEstablishmentDto dto) {
+        if(dto.name() != null)
+            this.name = dto.name();
+
+        if(dto.photo() != null)
+            this.photo = dto.photo();
+
+        if(dto.description() != null)
+            this.description = dto.description();
+
+        if(dto.telephone() != null)
+            this.telephone = dto.telephone();
+
+        if(dto.cellphone() != null)
+            this.cellphone = dto.cellphone();
+
+        if(email != null)
+            this.email = dto.email();
+
+        if(category != null)
+            this.category = dto.category();
+
+        if(picture1 != null)
+            this.picture1 = dto.picture1();
+
+        if(picture2 != null)
+            this.picture2 = dto.picture2();
+
+        if(picture3 != null)
+            this.picture3 = dto.picture3();
+
+        if(picture4 != null)
+            this.picture4 = dto.picture4();
+
+        if(picture5 != null)
+            this.picture5 = dto.picture5();
+
+        if(picture6 != null)
+            this.picture6 = dto.picture6();
+
+        if(picture7 != null)
+            this.picture7 = dto.picture7();
+
+        if(picture8 != null)
+            this.picture8 = dto.picture8();
+
+        if(picture9 != null)
+            this.picture9 = dto.picture9();
+
+        if(picture10 != null)
+            this.picture10 = dto.picture10();
+    }
+}
