4157298e198780eec8229f6300911d922419ab71
daniel-fonseca-silva <daniel.silva@ext.leroymerlin.pt>
2024-12-30 01:07:02 +0000

    fix: Changed main because the infrastructure

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,54 @@
 package main
 
 import (
-	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/controller"
-	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/db"
-	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/repository"
-	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/usecase"
-	"github.com/gin-gonic/gin"
+	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/config"
+	"github.com/Daniel-Fonseca-da-Silva/Tr-Search-Back/router"
 )
 
-func main() {
-	server := gin.Default()
+var (
+	logger *config.Logger
+)
 
-	dbConnection, err := db.ConnectDB()
+// func init() {
+// 	err := godotenv.Load("/home/dafon/Study/Tr-Search-Back/.env")
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// }
+
+func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
 	if err != nil {
-		panic(err)
+		logger.Errorf("Config initilization error: %v", err)
+		return
 	}
 
+	router.Initialize()
+
+	// dbConnection, err := db.ConnectDB()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 	// Layer repository
-	ProductRepository := repository.NewProductRepository(dbConnection)
+	// ProductRepository := repository.NewProductRepository(dbConnection)
 
 	// Layer usercase
-	ProductUseCase := usecase.NewProductUsecase(ProductRepository)
+	// ProductUseCase := usecase.NewProductUsecase(ProductRepository)
 
 	// Layer controller
-	ProductController := controller.NewProductController(ProductUseCase)
+	// ProductController := controller.NewProductController(ProductUseCase)
 
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	// server.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"message": "pong"})
+	// })
 
-	server.GET("/products", ProductController.GetProducts)
-	server.POST("/product", ProductController.CreateProduct)
-	server.GET("/product/:productId", ProductController.GetProductById)
+	// server.GET("/products", ProductController.GetProducts)
+	// server.POST("/product", ProductController.CreateProduct)
+	// server.GET("/product/:productId", ProductController.GetProductById)
 
-	server.Run(":8000")
+	// server.Run(":8000")
 }
