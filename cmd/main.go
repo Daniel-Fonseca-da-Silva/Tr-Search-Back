ec22856c10521daab99eb2fec7b89d4fd999d809
daniel-fonseca-silva <daniel.silva@ext.leroymerlin.pt>
2024-12-30 01:07:18 +0000

    fix: Changed main because the infrastructure

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,6 @@ var (
 	logger *config.Logger
 )
 
-// func init() {
-// 	err := godotenv.Load("/home/dafon/Study/Tr-Search-Back/.env")
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
-// }
-
 func main() {
 
 	logger = config.GetLogger("main")
@@ -27,28 +20,4 @@ func main() {
 	}
 
 	router.Initialize()
-
-	// dbConnection, err := db.ConnectDB()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// Layer repository
-	// ProductRepository := repository.NewProductRepository(dbConnection)
-
-	// Layer usercase
-	// ProductUseCase := usecase.NewProductUsecase(ProductRepository)
-
-	// Layer controller
-	// ProductController := controller.NewProductController(ProductUseCase)
-
-	// server.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{"message": "pong"})
-	// })
-
-	// server.GET("/products", ProductController.GetProducts)
-	// server.POST("/product", ProductController.CreateProduct)
-	// server.GET("/product/:productId", ProductController.GetProductById)
-
-	// server.Run(":8000")
 }
